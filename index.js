20f50ee06161e23fbde54a16060d337b4d0fdd70
lemanh97 <dieuhien4996@gmail.com>
2023-06-29 08:22:13 +0700

    cronjob

---
+++ index.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const cron = require('cron');
+
+// Các URL cần gửi yêu cầu
+const urls = [
+  'http://127.0.0.1:8000/cronjob/get-comment-momo',
+  'http://127.0.0.1:8000/cronjob/history-momo'
+];
+
+// Hàm thực hiện yêu cầu HTTP
+const sendRequest = async (url) => {
+  try {
+    const response = await axios.get(url);
+    console.log(`Response from ${url}:`, response.data);
+  } catch (error) {
+    console.error(`Error while requesting ${url}:`, error.message);
+  }
+};
+
+// Tạo cron job chạy mỗi giây
+const job = new cron.CronJob('*/3 * * * * *', () => {
+  // Gửi yêu cầu tới mỗi URL trong danh sách
+  urls.forEach(async (url) => {
+    await sendRequest(url);
+  });
+});
+
+// Bắt đầu cron job
+job.start();
